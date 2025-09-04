d943e6d6af28cdf9abdf3a8c6c92aac66d52397f
Gonzalo Gomez Guerrero <143089392+GonzaloGG91@users.noreply.github.com>
2025-09-04 11:26:44 +0300

    Update producer.py

--- producer.py
+++ producer.py
@@ -8,11 +8,14 @@ from faker import Faker
 fake= Faker()
 # Change this to change the number of data that we are making up
 Number_of_rows=1000
+# Set up the conditions for Kafka to stream the data to a Kafka topic.
+# Connects to the local Kafka broker and serializes messages as JSON
 producer = KafkaProducer(
     bootstrap_servers="localhost:9092",
     value_serializer= lambda v:json.dumps(v).encode("utf-8")
 )
 def generate_athlete_event():
+    #Generates a lime of fake data
     return {
         "athlete_id": random.randint(100000,999999),
         "name": random.choice(["100m","200m", "LongJump", "Marathon", "NA"]),
@@ -22,13 +25,17 @@ def generate_athlete_event():
     }
 
 with open("stream.log","a") as f:
+    # Generates the amount of data needed and then send it to Kafka
+    # Also add each event to a local lof file for references
+    # writes a line every 100ms
     for _ in range(Number_of_rows):
 
         event= generate_athlete_event()
-        producer.send("athlete_events", value = event)
+        producer.send("athlete_events", value = event) # send to kafka
         print(f"Produced: {event}")
-        f.write(json.dumps(event) +"\n")
+        f.write(json.dumps(event) +"\n") # Local writing
         time.sleep(0.1)
 
 producer.flush()
-producer.close()
+
+producer.close() #closes producer
